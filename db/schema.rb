c915d95dcdf9443df856a6ef7f7a0853f130d20f
Olivier <olivier.simart@gmail.com>
2019-12-08 14:44:53 -0500

    Drop users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_30_130752) do
+ActiveRecord::Schema.define(version: 2019_12_08_194349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
@@ -39,27 +39,6 @@ ActiveRecord::Schema.define(version: 2019_10_30_130752) do
 
   create_table "series", id: :serial, force: :cascade do |t|
     t.string "name"
-  end
-
-  create_table "users", id: :serial, force: :cascade do |t|
-    t.string "email"
-    t.string "first_name"
-    t.string "last_name"
-    t.boolean "admin", default: false
-    t.string "localization", default: "fr"
-    t.string "crypted_password"
-    t.string "salt"
-    t.text "default_header"
-    t.text "default_footer"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string "remember_me_token"
-    t.datetime "remember_me_token_expires_at"
-    t.string "reset_password_token"
-    t.datetime "reset_password_token_expires_at"
-    t.datetime "reset_password_email_sent_at"
-    t.index ["remember_me_token"], name: "index_users_on_remember_me_token"
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
   end
 
 end
