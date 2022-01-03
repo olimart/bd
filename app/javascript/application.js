948e3f7d1526422a856962ca5d3db8934da4e360
olimart <olimart@users.noreply.github.com>
2022-01-03 12:50:47 -0500

    More assets cleanup

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,17 +1,14 @@
 // Entry point for the build script in your package.json
 // import * as bootstrap from "bootstrap"
 
-// import Rails from "@rails/ujs"
 import * as Turbo from "@hotwired/turbo"
 
 // Make accessible for Electron and Mobile adapters
-// window.Rails = Rails
 window.Turbo = Turbo
 
 // import * as ActiveStorage from "@rails/activestorage"
 // import "./channels"
 
-// Rails.start()
 // ActiveStorage.start()
 
 import "./controllers"
