152fff1d49c0d16d84ea22daa1d2637dc6b52134
olimart <olimart@users.noreply.github.com>
2022-01-12 22:57:36 -0500

    Bump Rails 7.0.1

--- config/initializers/queenbee.rb
+++ config/initializers/queenbee.rb
@@ -1,4 +1,4 @@
 Queenbee.token = Rails.application.credentials.queenbee
 if Rails.env.development?
-  Queenbee.api_base = 'http://localhost:3024/api'
+  Queenbee.api_base = "http://localhost:3024/api"
 end
